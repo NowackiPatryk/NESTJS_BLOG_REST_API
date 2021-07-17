3c457cfd2f43454608a4fc52776da27d90481419
Jan Kamienicki <nowak480@gmail.com>
2021-07-17 23:39:47 +0200

    added hashing via bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +13,11 @@ export class AuthService {
   async validateUser(username: string, plaintextPass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
 
-    if (user && user.password === plaintextPass) {
+    if (user) {
+      const compareResult = await bcrypt.compare(plaintextPass, user.password);
+      if (!compareResult) {
+        return null;
+      }
       const { password, ...result } = user;
       return result;
     }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from './users.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -15,6 +16,9 @@ export class UsersService {
   }
 
   async create(createUserDto: CreateUserDto): Promise<any> {
-    return this.usersRepository.save(createUserDto);
+    const { username, password } = createUserDto;
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    return this.usersRepository.save({ username, password: hashedPass });
   }
 }
